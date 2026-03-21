01be1b38b1b5a0bbefa2563cb2a0ce1d4dac7d6f
egg1245 <danilpere1010@gmail.com>
2026-03-21 15:18:59 +0300

    Fix __main__.py PYTHONPATH

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-"""Allow running as: python -m bot --test "/command" from bot/ directory"""
+"""Allow running as: python -m bot --test "/command" from bot/ directory
+Or: uv run python -m bot --test "/command"
+"""
 
 import sys
 import os
 
-# Ensure parent directory is in path for imports from bot/
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if parent_dir not in sys.path:
-    sys.path.insert(0, parent_dir)
+# Add parent directory so 'bot' package can be found
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# Now import from bot.bot
 from bot.bot import main
 
 if __name__ == "__main__":
